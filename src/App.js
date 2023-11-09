cb59ecea867def081f126f29d76c0a564f4f2b9a
Prabhat Dutt <prabhatdutt95@gmail.com>
2023-11-09 18:40:46 +0530

    Package, Action-creator & Middlewares
    - Added required packages.
    - Added required Action-creators.
    - Added required middlewares.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { useEffect } from 'react';
 import './App.css';
 
-function App() {
+// Action handler
+import { handleInitialData } from './actions/shared';
+
+const App = (props) =>  {
+  const handleInitData = () => {
+    props.dispatch(handleInitialData)
+  }
+  useEffect(() => {
+    handleInitData();
+  }, [])
+  
   return (
     <div>Sample</div>
   );
---
+++ src/actions/shared.js
@@ -0,0 +1,21 @@
+import { getInitialData } from "../utils/api";
+import { showLoading, hideLoading } from "react-redux-loading-bar";
+
+// Importing Action Creators
+import { receiveQuestions } from "./questions";
+import { receiveUsers } from "./users";
+import { setAuthedUser } from "./authedUser"; 
+
+const AUTHED_ID = "tylermcginnis";
+
+export function handleInitialData() {
+    return (dispatch) => {
+        dispatch(showLoading());
+        return getInitialData().then(({users, questions}) => {
+            dispatch(receiveUsers(users));
+            dispatch(receiveQuestions(questions));
+            dispatch(setAuthedUser(AUTHED_ID));
+            dispatch(hideLoading());
+        })
+    }
+}
